3987f5985b50d9141194a48051e3388bb9dd09ba
crunis89 <crunis89@gmail.com>
2020-01-16 16:26:07 +0900

    day2.js change of content

--- day2.js
+++ day2.js
@@ -7,3 +7,18 @@ if (a === 5) {
 } else {
     console.log('a는 5도아니고 10도 아닙니다')
 }
+
+// if else if else
+
+const device = 'tv';
+
+switch (device) {
+    case 'samsung':
+        console.log('삼성꺼다!')
+        break;
+    case 'lg':
+        console.log('lg꺼다!')
+        break;
+    default:
+        console.log('해당사항이 없습니다!');
+}
